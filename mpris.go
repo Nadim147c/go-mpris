869c4ef712606b9f071be0837c25ab091c6cd10e
Ephemeral <theephemeral.txt@gmail.com>
2025-09-26 21:34:32 +0600

    feat: implements all player interface

--- mpris.go
+++ mpris.go
@@ -1,7 +1,6 @@
 package mpris
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
@@ -51,171 +50,9 @@ func (i *Player) GetName() string {
 	return i.name
 }
 
-// CanPlay returns if player can play
-func (i *Player) CanPlay() (bool, error) {
-	return getPlayerPropertyCast(i, "CanPlay", cast.ToBoolE)
-}
-
-// CanPause returns if player can pause
-func (i *Player) CanPause() (bool, error) {
-	return getPlayerPropertyCast(i, "CanPause", cast.ToBoolE)
-}
-
-// CanSeek returns if player can seek
-func (i *Player) CanSeek() (bool, error) {
-	return getPlayerPropertyCast(i, "CanSeek", cast.ToBoolE)
-}
-
-// CanControl returns if player can control
-func (i *Player) CanControl() (bool, error) {
-	return getPlayerPropertyCast(i, "CanControl", cast.ToBoolE)
-}
-
-// CanGoNext returns if player can go next
-func (i *Player) CanGoNext() (bool, error) {
-	return getPlayerPropertyCast(i, "CanGoNext", cast.ToBoolE)
-}
-
-// CanGoPrevious returns if player can go previous
-func (i *Player) CanGoPrevious() (bool, error) {
-	return getPlayerPropertyCast(i, "CanGoPrevious", cast.ToBoolE)
-}
-
 // CanEditTracks returns if player can edit track list
 func (i *Player) CanEditTracks() (bool, error) {
 	return getTrackListPropertyCast(i, "CanEditTracks", cast.ToBoolE)
-}
-
-// Next skips to the next track in the tracklist.
-func (i *Player) Next() error {
-	return i.obj.Call(PlayerInterface+".Next", 0).Err
-}
-
-// Previous skips to the previous track in the tracklist.
-func (i *Player) Previous() error {
-	return i.obj.Call(PlayerInterface+".Previous", 0).Err
-}
-
-// Pause pauses the current track.
-func (i *Player) Pause() error {
-	return i.obj.Call(PlayerInterface+".Pause", 0).Err
-}
-
-// PlayPause resumes the current track if it's paused and pauses it if it's playing.
-func (i *Player) PlayPause() error {
-	return i.obj.Call(PlayerInterface+".PlayPause", 0).Err
-}
-
-// Stop stops the current track.
-func (i *Player) Stop() error {
-	return i.obj.Call(PlayerInterface+".Stop", 0).Err
-}
-
-// Play starts or resumes the current track.
-func (i *Player) Play() error {
-	return i.obj.Call(PlayerInterface+".Play", 0).Err
-}
-
-// Seek seeks the current track position by the offset.
-// If the offset is negative it's seeked back.
-func (i *Player) Seek(offset time.Duration) error {
-	oms := offset.Microseconds()
-	return i.obj.Call(PlayerInterface+".Seek", 0, oms).Err
-}
-
-// SetTrackPosition sets the position of a track.
-func (i *Player) SetTrackPosition(trackId *dbus.ObjectPath, position time.Duration) error {
-	oms := position.Microseconds()
-	return i.obj.Call(PlayerInterface+".SetPosition", 0, trackId, oms).Err
-}
-
-// OpenUri opens and plays the uri if supported.
-//
-// Deprecated: Use OpenURI instead of OpenUri
-func (i *Player) OpenUri(uri string) error {
-	return i.OpenURI(uri)
-}
-
-// OpenURI opens and plays the uri if supported.
-func (i *Player) OpenURI(uri string) error {
-	return i.obj.Call(PlayerInterface+".OpenUri", 0, uri).Err
-}
-
-// PlaybackStatus the status of the playback. It can be "Playing", "Paused" or "Stopped".
-type PlaybackStatus string
-
-const (
-	PlaybackPlaying PlaybackStatus = "Playing"
-	PlaybackPaused  PlaybackStatus = "Paused"
-	PlaybackStopped PlaybackStatus = "Stopped"
-)
-
-// GetPlaybackStatus gets the playback status.
-func (i *Player) GetPlaybackStatus() (PlaybackStatus, error) {
-	str, err := getPlayerPropertyCast(i, "PlaybackStatus", cast.ToStringE)
-	return PlaybackStatus(str), err
-}
-
-// LoopStatus the status of the player loop. It can be "None", "Track" or "Playlist".
-type LoopStatus string
-
-const (
-	LoopNone     LoopStatus = "None"
-	LoopTrack    LoopStatus = "Track"
-	LoopPlaylist LoopStatus = "Playlist"
-)
-
-// GetLoopStatus returns the loop status.
-func (i *Player) GetLoopStatus() (LoopStatus, error) {
-	str, err := getPlayerPropertyCast(i, "LoopStatus", cast.ToStringE)
-	return LoopStatus(str), err
-}
-
-// SetLoopStatus sets the loop status to loopStatus.
-func (i *Player) SetLoopStatus(loopStatus LoopStatus) error {
-	return i.SetPlayerProperty("LoopStatus", loopStatus)
-}
-
-// Returns the current playback rate.
-func (i *Player) GetRate() (float64, error) {
-	return getPlayerPropertyCast(i, "Rate", cast.ToFloat64E)
-}
-
-// GetShuffle returns false if the player is going linearly through a playlist and true if it's
-// in some other order.
-func (i *Player) GetShuffle() (bool, error) {
-	return getPlayerPropertyCast(i, "Shuffle", cast.ToBoolE)
-}
-
-// SetShuffle sets the shuffle playlist mode.
-func (i *Player) SetShuffle(value bool) error {
-	return i.SetPlayerProperty("Shuffle", value)
-}
-
-type Metadata map[string]dbus.Variant
-
-func (m Metadata) Get(key string) (any, error) {
-	v, ok := m[key]
-	if !ok || v.Value() == nil {
-		return v, fmt.Errorf("%s.Metadata missing or nil for key %q", PlayerInterface, key)
-	}
-	return v.Value(), nil
-}
-
-// GetMetadata returns the metadata.
-func (i *Player) GetMetadata() (Metadata, error) {
-	return getPlayerPropertyCast(i, "Metadata", func(a any) (Metadata, error) {
-		v, ok := a.(map[string]dbus.Variant)
-		if !ok {
-			return Metadata{}, fmt.Errorf("failed to cast %s.Metadata value (%v) to map[string]dbus.Variant", PlayerInterface, a)
-		}
-		return Metadata(v), nil
-	})
-}
-
-// GetVolume returns the volume.
-func (i *Player) GetVolume() (float64, error) {
-	return getPlayerPropertyCast(i, "Volume", cast.ToFloat64E)
 }
 
 // GetLength returns the current track length.
@@ -224,25 +61,10 @@ func (i *Player) GetLength() (time.Duration, error) {
 	return time.Duration(micro) * time.Microsecond, err
 }
 
-// GetPosition returns the position of the current track.
-func (i *Player) GetPosition() (time.Duration, error) {
-	micro, err := getPlayerPropertyCast(i, "Position", cast.ToInt64E)
-	return time.Duration(micro) * time.Microsecond, err
-}
-
 // GetTrackID returns track id for player as dbus.ObjectPath
 func (i *Player) GetTrackID() (dbus.ObjectPath, error) {
 	trackIdStr, err := getMetadataCast(i, "mpris:trackid", cast.ToStringE)
 	return dbus.ObjectPath(trackIdStr), err
-}
-
-// SetPosition sets the position of the current track.
-func (i *Player) SetPosition(position time.Duration) error {
-	trackID, err := i.GetTrackID()
-	if err != nil {
-		return err
-	}
-	return i.SetTrackPosition(&trackID, position)
 }
 
 // GetTitle returns the current track title.
@@ -268,11 +90,6 @@ func (i *Player) GetURL() (string, error) {
 // GetCoverURL returns the cover art URL of the current track.
 func (i *Player) GetCoverURL() (string, error) {
 	return getMetadataCast(i, "mpris:artUrl", cast.ToStringE)
-}
-
-// SetVolume sets the volume.
-func (i *Player) SetVolume(volume float64) error {
-	return i.SetPlayerProperty("Volume", volume)
 }
 
 // New connects the the player with the name in the connection conn.
---
+++ player.go
@@ -0,0 +1,213 @@
+package mpris
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/spf13/cast"
+)
+
+// Methods
+
+// Next skips to the next track in the tracklist.
+func (i *Player) Next() error {
+	return i.obj.Call(PlayerInterface+".Next", 0).Err
+}
+
+// Previous skips to the previous track in the tracklist.
+func (i *Player) Previous() error {
+	return i.obj.Call(PlayerInterface+".Previous", 0).Err
+}
+
+// Pause pauses the current track.
+func (i *Player) Pause() error {
+	return i.obj.Call(PlayerInterface+".Pause", 0).Err
+}
+
+// PlayPause resumes the current track if it's paused and pauses it if it's playing.
+func (i *Player) PlayPause() error {
+	return i.obj.Call(PlayerInterface+".PlayPause", 0).Err
+}
+
+// Stop stops the current track.
+func (i *Player) Stop() error {
+	return i.obj.Call(PlayerInterface+".Stop", 0).Err
+}
+
+// Play starts or resumes playback of the current track.
+func (i *Player) Play() error {
+	return i.obj.Call(PlayerInterface+".Play", 0).Err
+}
+
+// Seek changes the current track position by the given offset.
+// If the offset is negative, the playback position moves backward.
+func (i *Player) Seek(offset time.Duration) error {
+	micro := offset.Microseconds()
+	return i.obj.Call(PlayerInterface+".Seek", 0, micro).Err
+}
+
+// SetTrackPosition sets the playback position of a specific track.
+func (i *Player) SetTrackPosition(trackId *dbus.ObjectPath, position time.Duration) error {
+	oms := position.Microseconds()
+	return i.obj.Call(PlayerInterface+".SetPosition", 0, trackId, oms).Err
+}
+
+// SetPosition sets the playback position of the current track.
+func (i *Player) SetPosition(position time.Duration) error {
+	trackID, err := i.GetTrackID()
+	if err != nil {
+		return err
+	}
+	return i.SetTrackPosition(&trackID, position)
+}
+
+// OpenUri opens and plays the given URI if supported.
+//
+// Deprecated: Use OpenURI instead.
+func (i *Player) OpenUri(uri string) error {
+	return i.OpenURI(uri)
+}
+
+// OpenURI opens and plays the given URI if supported.
+func (i *Player) OpenURI(uri string) error {
+	return i.obj.Call(PlayerInterface+".OpenUri", 0, uri).Err
+}
+
+// Properties
+
+// PlaybackStatus represents the playback status. It can be "Playing", "Paused" or "Stopped".
+type PlaybackStatus string
+
+const (
+	PlaybackPlaying PlaybackStatus = "Playing"
+	PlaybackPaused  PlaybackStatus = "Paused"
+	PlaybackStopped PlaybackStatus = "Stopped"
+)
+
+// GetPlaybackStatus returns the current playback status.
+func (i *Player) GetPlaybackStatus() (PlaybackStatus, error) {
+	str, err := getPlayerPropertyCast(i, "PlaybackStatus", cast.ToStringE)
+	return PlaybackStatus(str), err
+}
+
+// LoopStatus represents the loop status of the player. It can be "None", "Track" or "Playlist".
+type LoopStatus string
+
+const (
+	LoopNone     LoopStatus = "None"
+	LoopTrack    LoopStatus = "Track"
+	LoopPlaylist LoopStatus = "Playlist"
+)
+
+// GetLoopStatus returns the current loop status.
+func (i *Player) GetLoopStatus() (LoopStatus, error) {
+	str, err := getPlayerPropertyCast(i, "LoopStatus", cast.ToStringE)
+	return LoopStatus(str), err
+}
+
+// SetLoopStatus sets the loop status.
+func (i *Player) SetLoopStatus(loopStatus LoopStatus) error {
+	return i.SetPlayerProperty("LoopStatus", loopStatus)
+}
+
+// GetRate returns the current playback rate.
+func (i *Player) GetRate() (float64, error) {
+	return getPlayerPropertyCast(i, "Rate", cast.ToFloat64E)
+}
+
+// SetRate sets the playback rate.
+func (i *Player) SetRate(rate float64) error {
+	return i.SetPlayerProperty("Rate", rate)
+}
+
+// GetShuffle returns true if shuffle mode is enabled, false if playing linearly through a playlist.
+func (i *Player) GetShuffle() (bool, error) {
+	return getPlayerPropertyCast(i, "Shuffle", cast.ToBoolE)
+}
+
+// SetShuffle sets the shuffle mode.
+func (i *Player) SetShuffle(value bool) error {
+	return i.SetPlayerProperty("Shuffle", value)
+}
+
+// Metadata represents the metadata of the current track.
+type Metadata map[string]dbus.Variant
+
+// Get returns the value for the given metadata key.
+func (m Metadata) Get(key string) (any, error) {
+	v, ok := m[key]
+	if !ok || v.Value() == nil {
+		return v, fmt.Errorf("%s.Metadata missing or nil for key %q", PlayerInterface, key)
+	}
+	return v.Value(), nil
+}
+
+// GetMetadata returns the current track metadata.
+func (i *Player) GetMetadata() (Metadata, error) {
+	return getPlayerPropertyCast(i, "Metadata", func(a any) (Metadata, error) {
+		v, ok := a.(map[string]dbus.Variant)
+		if !ok {
+			return Metadata{}, fmt.Errorf(
+				"failed to cast %s.Metadata value (%v) to map[string]dbus.Variant",
+				PlayerInterface, a)
+		}
+		return Metadata(v), nil
+	})
+}
+
+// GetVolume returns the current volume.
+func (i *Player) GetVolume() (float64, error) {
+	return getPlayerPropertyCast(i, "Volume", cast.ToFloat64E)
+}
+
+// SetVolume sets the current volume.
+func (i *Player) SetVolume(volume float64) error {
+	return i.SetPlayerProperty("Volume", volume)
+}
+
+// GetPosition returns the current playback position.
+func (i *Player) GetPosition() (time.Duration, error) {
+	micro, err := getPlayerPropertyCast(i, "Position", cast.ToInt64E)
+	return time.Duration(micro) * time.Microsecond, err
+}
+
+// GetMinimumRate returns the minimum playback rate.
+func (i *Player) GetMinimumRate() (float64, error) {
+	return getPlayerPropertyCast(i, "MinimumRate", cast.ToFloat64E)
+}
+
+// GetMaximumRate returns the maximum playback rate.
+func (i *Player) GetMaximumRate() (float64, error) {
+	return getPlayerPropertyCast(i, "MaximumRate", cast.ToFloat64E)
+}
+
+// CanGoNext returns whether the player can skip to the next track.
+func (i *Player) CanGoNext() (bool, error) {
+	return getPlayerPropertyCast(i, "CanGoNext", cast.ToBoolE)
+}
+
+// CanGoPrevious returns whether the player can skip to the previous track.
+func (i *Player) CanGoPrevious() (bool, error) {
+	return getPlayerPropertyCast(i, "CanGoPrevious", cast.ToBoolE)
+}
+
+// CanPlay returns whether the player can start or resume playback.
+func (i *Player) CanPlay() (bool, error) {
+	return getPlayerPropertyCast(i, "CanPlay", cast.ToBoolE)
+}
+
+// CanPause returns whether the player can pause playback.
+func (i *Player) CanPause() (bool, error) {
+	return getPlayerPropertyCast(i, "CanPause", cast.ToBoolE)
+}
+
+// CanSeek returns whether the player can seek within the current track.
+func (i *Player) CanSeek() (bool, error) {
+	return getPlayerPropertyCast(i, "CanSeek", cast.ToBoolE)
+}
+
+// CanControl returns whether the player can be controlled.
+func (i *Player) CanControl() (bool, error) {
+	return getPlayerPropertyCast(i, "CanControl", cast.ToBoolE)
+}
